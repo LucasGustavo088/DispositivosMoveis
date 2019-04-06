7fde72a5a81ade6e5defa60cae7d906f25b01795
Lucas <lucas.gustavo088@gmail.com>
2019-04-06 19:08:25 -0300

    aula04

--- Aula02Entregas/app/src/main/java/br/com/bossini/usjt_ccp3anmca_ciclo_de_vida_gps_e_mapas/ListaLocalizacoesActivity.java
+++ Aula02Entregas/app/src/main/java/br/com/bossini/usjt_ccp3anmca_ciclo_de_vida_gps_e_mapas/ListaLocalizacoesActivity.java
@@ -8,12 +8,10 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,19 +20,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 public class ListaLocalizacoesActivity extends AppCompatActivity {
 
-    private ListView chamadosListView;
+    private ListView localizacoesListView;
     private TextView localizacaoTextView;
     private Location localizacaoAtual;
     private LocationManager locationManager;
     private LocationListener locationListener;
     private static final int REQUEST_CODE_GPS = 1001;
-    ArrayList<String> lista = new ArrayList<>();
+    List <Localizacao> lista = new ArrayList<>();
 
     public class LatLong {
         public double latitude;
@@ -42,28 +39,21 @@ public class ListaLocalizacoesActivity extends AppCompatActivity {
     }
 
     public void atualizarView() {
-        atualizarLocalizacoes();
-        List <String> chamados = lista;
-        chamadosListView = findViewById(R.id.chamadosListView);
-        chamadosListView.setAdapter(null);
-//        Collections.reverse(chamados);
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chamados);
-        chamadosListView.setAdapter(adapter);
+        gerarLocalizacoes();
+        localizacoesListView = findViewById(R.id.chamadosListView);
+        localizacoesListView.setAdapter(null);
 
+        ArrayAdapter<Localizacao> adapter = new LocalizacaoArrayAdapter(this, lista);
+        localizacoesListView.setAdapter(adapter);
 
-        chamadosListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        localizacoesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 TextView textView = view.findViewById(R.id.chamadosListView);
-                String selectedFromList = (String) chamadosListView.getItemAtPosition(position);
-                selectedFromList = selectedFromList.replace("Lat: ", "").replace(" Long: ", "");
+                Localizacao selectedFromList = lista.get(position);
 
-                double latitude = Double.parseDouble(selectedFromList.split(",")[0]);
-                double longitude = Double.parseDouble(selectedFromList.split(",")[1]);
-                Log.d(selectedFromList, selectedFromList);
                 Uri uri = Uri.parse(
-                        String.format("geo:%f,%f", latitude, longitude));
+                        String.format("geo:%f,%f", selectedFromList.getLatitude(), selectedFromList.getLongitude()));
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 intent.setPackage("com.google.android.apps.maps");
                 startActivity(intent);
@@ -76,57 +66,37 @@ public class ListaLocalizacoesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_localizacoes);
 
-
-
-
         locationManager =
                 (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        Log.d("antesmudou","antesmudou");
         locationListener = new LocationListener() {
 
             @Override
             public void onLocationChanged(Location location) {
-                Log.d("onLocationChanged","onLocationChanged");
                 localizacaoAtual = location;
-                /*double lat = location.getLatitude();
-                double lon = location.getLongitude();
-                localizacaoTextView.setText(
-                        String.format("Lat: %f, Long: %f", lat, lon)
-                );*/
+
                 atualizarView();
             }
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
-                Log.d("onStatusChanged", "onStatusChanged");
+
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-                Log.d("onProviderEnabled", "onProviderEnabled");
+
             }
 
             @Override
             public void onProviderDisabled(String provider) {
-                Log.d("onProviderDisabled", "onProviderDisabled");
+
             }
         };
 
-//        final Handler handler = new Handler();
-//        handler.post(new Runnable(){
-//            @Override
-//            public void run() {
-//                Log.d("atualizado", "atualizado");
-//                atualizarView();
-//                handler.postDelayed(this,3000); // set time here to refresh textView
-//            }
-//        });
     }
 
-    public void atualizarLocalizacoes(){
-
-
+    public void gerarLocalizacoes(){
 
         if(lista.isEmpty()) {
             for(int i = 0; i < 50; i++) {
@@ -138,11 +108,14 @@ public class ListaLocalizacoesActivity extends AppCompatActivity {
                     lon = localizacaoAtual.getLongitude();
                 }
 
-//                lat = this.obterLatitudeLongitude().latitude;
-//                lon = this.obterLatitudeLongitude().longitude;
-
-//                Log.d("populando", "teste");
-                lista.add(String.format("Lat: %f, Long: %f", lat, lon));
+                //lista.add(String.format("Lat: %f, Long: %f", lat, lon));
+                lista.add(
+                        new Localizacao (
+                                new Fila ("Nome da fila"),
+                                lat,
+                                lon,
+                                new Date()
+                        ));
             }
         } else {
 
@@ -153,30 +126,18 @@ public class ListaLocalizacoesActivity extends AppCompatActivity {
                 lon = localizacaoAtual.getLongitude();
             }
 
-//            lat = this.obterLatitudeLongitude().latitude;
-//            lon = this.obterLatitudeLongitude().longitude;
+            lista.add(
+                    new Localizacao (
+                            new Fila ("Nome da fila"),
+                            lat,
+                            lon,
+                            new Date()
+                    ));
 
-            lista.add(0, String.format("Lat: %f, Long: %f", lat, lon));
+            //lista.add(0, String.format("Lat: %f, Long: %f", lat, lon));
             lista.remove(lista.size() - 1);
         }
-
-//        int listaid = 0;
-//        for(String list : lista) {
-//            Log.d("Lista: " + listaid++, list);
-//        }
-
     }
-
-    public LatLong obterLatitudeLongitude() {
-        LatLong latlong = new LatLong();
-        Random r = new Random();
-        latlong.latitude = r.nextDouble();
-        latlong.longitude = r.nextDouble();
-
-        return latlong;
-    }
-
-
 
     @Override
     protected void onStop(){
@@ -211,7 +172,6 @@ public class ListaLocalizacoesActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("porcaria","porcaria");
 
         if (ActivityCompat.
                 checkSelfPermission(this,
